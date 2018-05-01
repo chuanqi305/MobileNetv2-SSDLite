9bfe19e1e726d245866fb1166d05460f68969d31
chuanqi305 <yaochuanqi888@163.com>
2018-05-01 11:41:42 +0800

    display class names

--- demo_caffe.py
+++ demo_caffe.py
@@ -13,12 +13,22 @@ test_dir = "images"
 caffe.set_mode_gpu()
 net = caffe.Net(net_file,caffe_model,caffe.TEST)  
 
-CLASSES = ('background',
-           'aeroplane', 'bicycle', 'bird', 'boat',
-           'bottle', 'bus', 'car', 'cat', 'chair',
-           'cow', 'diningtable', 'dog', 'horse',
-           'motorbike', 'person', 'pottedplant',
-           'sheep', 'sofa', 'train', 'tvmonitor')
+CLASSES = ("backgroud" , "person" , "bicycle" , "car" , "motorcycle" , 
+     "airplane" , "bus" , "train" , "truck" , "boat" , "traffic light",
+     "fire hydrant", "N/A" , "stop sign", "parking meter", "bench" , 
+     "bird" , "cat" , "dog" , "horse" , "sheep" , "cow" , "elephant" , 
+     "bear" , "zebra" , "giraffe" , "N/A" , "backpack" , "umbrella" , 
+     "N/A" , "N/A" , "handbag" , "tie" , "suitcase" , "frisbee" , "skis" ,
+     "snowboard" , "sports ball", "kite" , "baseball bat", "baseball glove",
+     "skateboard" , "surfboard" , "tennis racket", "bottle" , "N/A" ,
+     "wine glass", "cup" , "fork" , "knife" , "spoon" , "bowl" , "banana" ,
+     "apple" , "sandwich" , "orange" , "broccoli" , "carrot" , "hot dog",
+     "pizza" , "donut" , "cake" , "chair" , "couch" , "potted plant", 
+     "bed" , "N/A" , "dining table", "N/A" , "N/A" , "toilet" , "N/A" ,
+     "tv" , "laptop" , "mouse" , "remote" , "keyboard" , "cell phone",
+     "microwave" , "oven" , "toaster" , "sink" , "refrigerator" , "N/A" ,
+     "book" , "clock" , "vase" , "scissors" , "teddy bear", "hair drier",
+     "toothbrush" )
 
 
 def preprocess(src):
@@ -52,7 +62,7 @@ def detect(imgfile):
        p2 = (box[i][2], box[i][3])
        cv2.rectangle(origimg, p1, p2, (0,255,0))
        p3 = (max(p1[0], 15), max(p1[1], 15))
-       title = "%s:%.2f" % (str(int(cls[i])), conf[i])
+       title = "%s:%.2f" % (CLASSES[int(cls[i])], conf[i])
        cv2.putText(origimg, title, p3, cv2.FONT_ITALIC, 0.6, (0, 255, 0), 1)
     cv2.imshow("SSD", origimg)
  
